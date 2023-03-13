7eee80f1630273d3294f102a215f287002546c9f
L. Carolina Arias <joseluy123@gmail.com>
2023-03-13 20:45:22 +0100

    config next

--- next.config.js
+++ next.config.js
@@ -3,13 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ['dvvjkgh94f2v6.cloudfront.net','images.unsplash.com','unsplash.com','pfsrealty.com'],
-  },
-  publicRuntimeConfig: {
-    // Define your environment variables here
-    API_URL: process.env.API_URL || 'http://localhost:3000'
-  },
-  basePath: '',
-  assetPrefix: '',
+  }
 }
 
 module.exports = nextConfig
