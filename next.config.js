45980618c9a7adeea9b512b21347878c85bd1cbc
L. Carolina Arias <joseluy123@gmail.com>
2023-03-13 18:50:51 +0100

    repair fetch file lib-data

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: ['dvvjkgh94f2v6.cloudfront.net','images.unsplash.com','unsplash.com','pfsrealty.com'],
   },
+  publicRuntimeConfig: {
+    // Define your environment variables here
+    API_URL: process.env.API_URL || 'http://localhost:3000'
+  }
 }
 
 module.exports = nextConfig
